c23cccca04cc676db66d715a98da59578be3deb6
Henrik Solgaard <h@solg.no>
2020-02-05 01:52:01 +0100

    Inital version with query execution and table listing.

---
+++ src/main/java/no/solg/sqliteclient/App.java
@@ -0,0 +1,95 @@
+package no.solg.sqliteclient;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.sqlite.SQLiteConfig;
+
+public class App {
+    static void executeQuery(String sql, Connection conn) {
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            ResultSetMetaData meta = rs.getMetaData();
+            while (rs.next()) {
+                int count = meta.getColumnCount();
+                for (int i=1; i<=count; i++) {
+                    String val = rs.getString(i);
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void listTables(Connection conn) {
+        try {
+            ResultSet rs = conn.getMetaData().getTables(null, null, null, null);
+            while (rs.next()) {
+                System.out.println(rs.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: sqlitequery.jar <database file>");
+        } else {
+            final String dbPath = args[0];
+            final String filename = new File(dbPath).getName();
+            String dbName;
+            if (filename.indexOf('.') != -1) {
+                dbName = filename.substring(0, filename.indexOf('.'));
+            } else {
+                dbName = filename;
+            }
+            SQLiteConfig config = new SQLiteConfig();
+            config.setReadOnly(true);
+            try {
+                Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath, config.toProperties());
+
+                Terminal terminal = TerminalBuilder.terminal();
+                LineReader lineReader = LineReaderBuilder.builder()
+                        .terminal(terminal)
+                        .build();
+                final String prompt = dbName + "> ";
+                while (true) {
+                    try {
+                        final String sql = lineReader.readLine(prompt);
+                        if (sql.equalsIgnoreCase(".quit")) {
+                            break;
+                        } else if (sql.equalsIgnoreCase(".tables")) {
+                            listTables(conn);
+                        } else if (sql.startsWith(".")) {
+                            System.out.println("Unknown command: " + sql);
+                        } else {
+                            executeQuery(sql, conn);
+                        }
+                    } catch (UserInterruptException e) {
+                        break;
+                    } catch (EndOfFileException e) {
+                        break;
+                    }
+                }
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
